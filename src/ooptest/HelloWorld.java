1c253f521407430493431754d10338be949851ab
samPuriwat <sam.puriwat@gmail.com>
2017-11-21 15:22:39 +0700

    Testing Edit Data

--- src/ooptest/HelloWorld.java
+++ src/ooptest/HelloWorld.java
@@ -8,8 +8,8 @@ public class HelloWorld {
 //        Add Data
         System.out.println("Puriwat Lertkrai");
         System.out.println("Information System");
-        System.out.println("Faculty of Management Technology");
-        System.out.println("RMUTSV");
+        System.out.println("Faculty of Management Technology,RMUTSV");
+
     }//main
 
 }// class
