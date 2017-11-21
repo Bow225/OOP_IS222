bd3f6f58d39a33b24311d503f6019ab6e3e7f147
samPuriwat <sam.puriwat@gmail.com>
2017-11-21 15:19:53 +0700

    Testing Add Data

--- src/ooptest/HelloWorld.java
+++ src/ooptest/HelloWorld.java
@@ -5,7 +5,11 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello world, I'm SAM");
-
+//        Add Data
+        System.out.println("Puriwat Lertkrai");
+        System.out.println("Information System");
+        System.out.println("Faculty of Management Technology");
+        System.out.println("RMUTSV");
     }//main
 
 }// class
