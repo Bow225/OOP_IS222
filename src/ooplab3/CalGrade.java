f87422fd6e71098b93fddcb91e28e985729c1831
samPuriwat <sam.puriwat@gmail.com>
2017-11-29 11:37:26 +0700

    Grade Calculator with condition

--- src/ooplab3/CalGrade.java
+++ src/ooplab3/CalGrade.java
@@ -12,16 +12,27 @@ public class CalGrade {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int score;
-        System.out.print("Enter your score: ");
-        score = scanner.nextInt();
+        int count =0;
+        for(int i=1;i<=3;i++) {
+            System.out.print("Enter your score (0-100): ");
+            score = scanner.nextInt();
+            if(score<0 || score>100){
+                count++;
+                if(count ==3) {
+                    System.out.println("System Error.");
+                    break;
+                }
+            }
+            else{
 //        test condition score
-        if (score < 49) System.out.println("Your grade is F");
-        else if (score < 59) System.out.println("Your grade is D");
-        else if (score < 69) System.out.println("Your grade is C");
-        else if (score < 79) System.out.println("Your grade is B");
-        else System.out.println("Your grade is A");
-
-
+                if (score < 49) System.out.println("Your grade is F");
+                else if (score < 59) System.out.println("Your grade is D");
+                else if (score < 69) System.out.println("Your grade is C");
+                else if (score < 79) System.out.println("Your grade is B");
+                else System.out.println("Your grade is A");
+                break;
+            }
+        }//for
     }//main
 
 
